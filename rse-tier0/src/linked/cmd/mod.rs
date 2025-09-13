0a21b596595f0307fa875fd5c036317b197b5820
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-09-13 19:35:50 +0300

    `rse-tier0`: Use `vt_object_wrapper!` for `LinkedTier0CommandLine`

--- rse-tier0/src/linked/cmd/mod.rs
+++ rse-tier0/src/linked/cmd/mod.rs
@@ -6,6 +6,8 @@ use ::core::{
 };
 use ::rse_cpp::{
 	VtObject, virtual_call,
+	vt_object_wrapper,
+	AsObject,
 };
 
 pub mod cppdef;
@@ -17,45 +19,52 @@ use crate::{
 
 use super::LinkedTier0;
 
-pub fn command_line() -> LinkedTier0CommandLine {
+vt_object_wrapper! {
+	pub struct LinkedTier0CommandLine for CommandLineVt;
+}
+impl AsObject<CommandLineVt> for &LinkedTier0CommandLine {
+	fn as_object(&self) -> &VtObject<CommandLineVt> {
+		&self.object
+	}
+}
+
+impl LinkedTier0CommandLine {
+	pub fn get() -> &'static Self {
+		unsafe { CommandLine_Tier0().cast().as_ref() }
+	}
+}
+
+pub fn command_line() -> &'static LinkedTier0CommandLine {
 	LinkedTier0CommandLine::get()
 }
 
 impl Tier0GetCommandLine for LinkedTier0 {
-	type CommandLine<'a> = LinkedTier0CommandLine where Self: 'a;
+	type CommandLine<'a> = &'static LinkedTier0CommandLine where Self: 'a;
 	fn command_line(&self) -> Self::CommandLine<'_> {
 		LinkedTier0CommandLine::get()
 	}
 }
 
-#[derive(Debug)]
-#[repr(transparent)]
-pub struct LinkedTier0CommandLine(&'static VtObject<CommandLineVt>);
-impl LinkedTier0CommandLine {
-	pub fn get() -> Self {
-		unsafe { Self(VtObject::from_ptr_const(CommandLine_Tier0())) }
-	}
-}
-impl Tier0CommandLine for LinkedTier0CommandLine {
+impl<T: AsObject<CommandLineVt>> Tier0CommandLine for T {
 	fn check_parm<'a>(&'a self, key: &CStr) -> Option<&'a CStr> {
 		let mut value = null();
 		unsafe {
-			virtual_call!(self.0 => check_parm(key.as_ptr(), &mut value));
+			virtual_call!(self.as_object() => check_parm(key.as_ptr(), &mut value));
 			c_str_opt(value)
 		}
 	}
 
 	fn parm_str<'a>(&'a self, key: &CStr) -> Option<&'a CStr> {
-		unsafe { c_str_opt(virtual_call!(self.0 => parm_value_str(key.as_ptr(), null()))) }
+		unsafe { c_str_opt(virtual_call!(self.as_object() => parm_value_str(key.as_ptr(), null()))) }
 	}
 	fn parm_str_or<'a>(&'a self, key: &CStr, default: &'a CStr) -> &'a CStr {
-		unsafe { CStr::from_ptr(virtual_call!(self.0 => parm_value_str(key.as_ptr(), default.as_ptr()))) }
+		unsafe { CStr::from_ptr(virtual_call!(self.as_object() => parm_value_str(key.as_ptr(), default.as_ptr()))) }
 	}
 	fn parm_int_or(&self, key: &CStr, default: c_int) -> c_int {
-		unsafe { virtual_call!(self.0 => parm_value_int(key.as_ptr(), default)) }
+		unsafe { virtual_call!(self.as_object() => parm_value_int(key.as_ptr(), default)) }
 	}
 	fn parm_float_or(&self, key: &CStr, default: c_float) -> c_float {
-		unsafe { virtual_call!(self.0 => parm_value_float(key.as_ptr(), default)) }
+		unsafe { virtual_call!(self.as_object() => parm_value_float(key.as_ptr(), default)) }
 	}
 }
 
