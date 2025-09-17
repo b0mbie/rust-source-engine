0e7b0f7110c474612798abaf2b0729bc46b11139
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-09-17 04:24:50 +0300

    `rse-utl`: Add docs for `CUtlVector`

--- rse-utl/src/cppdef/vector.rs
+++ rse-utl/src/cppdef/vector.rs
@@ -5,8 +5,11 @@ use super::UtlMemory;
 #[derive(Debug)]
 #[repr(C)]
 pub struct UtlVector<T, A = UtlMemory<T>> {
+	/// Memory that backs this vector.
 	pub memory: A,
+	/// Number of initialized elements in this vector.
 	pub size: c_int,
+	/// Field for easier access to the elements through a debugger.
 	#[cfg(not(feature = "xbox"))]
 	pub elements: *mut T,
 }
