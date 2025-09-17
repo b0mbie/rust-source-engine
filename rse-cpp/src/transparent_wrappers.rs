05e92cc2c810c4479d3fa390bca8df7f4577740c
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-09-17 03:54:17 +0300

    `rse-cpp`: Add `transparent_wrapper_impls!`

--- rse-cpp/src/transparent_wrappers.rs
+++ rse-cpp/src/transparent_wrappers.rs
@@ -1,4 +1,63 @@
 #[macro_export]
+macro_rules! transparent_wrapper_impls {
+	($name:ident for $target:ty as $target_name:literal) => {
+		/// Returns a mutable reference to a value of type
+		#[doc = concat!("[`", stringify!($name), "`]")]
+		/// given a raw pointer.
+		/// 
+		/// # Safety
+		/// `ptr` must point to a valid, mutable
+		#[doc = concat!("[`", $target_name, "`](", stringify!($target), ").")]
+		pub const unsafe fn from_mut_ptr<'a>(ptr: *mut $target) -> &'a mut Self {
+			unsafe { &mut *(ptr as *mut Self) }
+		}
+
+		/// Returns an immutable reference to a value of type
+		#[doc = concat!("[`", stringify!($name), "`]")]
+		/// given a raw pointer.
+		/// 
+		/// # Safety
+		/// `ptr` must point to a valid, immutable
+		#[doc = concat!("[`", $target_name, "`](", stringify!($target), ").")]
+		pub const unsafe fn from_ptr<'a>(ptr: *const $target) -> &'a Self {
+			unsafe { &*(ptr as *const Self) }
+		}
+
+		/// Returns the raw mutable pointer given a mutable reference to a value of type
+		#[doc = concat!("[`", stringify!($name), "`].")]
+		pub const fn as_mut_ptr(&mut self) -> *mut $target {
+			self as *mut Self as *mut $target
+		}
+
+		/// Returns the raw immutable pointer given an immutable reference to a value of type
+		#[doc = concat!("[`", stringify!($name), "`].")]
+		pub const fn as_ptr(&self) -> *const $target {
+			self as *const Self as *const $target
+		}
+
+		/// Returns an immutable reference to the inner C++ structure
+		/// given an immutable reference to a value of type
+		#[doc = concat!("[`", stringify!($name), "`].")]
+		pub const fn as_inner(&self) -> &$target {
+			&self.0
+		}
+
+		/// Returns a mutable reference to the inner C++ structure
+		/// given a mutable reference to a value of type
+		#[doc = concat!("[`", stringify!($name), "`].")]
+		///
+		/// See also [`as_inner`](Self::as_inner) for a safe version that doesn't permit mutation.
+		/// 
+		/// # Safety
+		/// The structure contains public fields for highly-specific applications,
+		/// which can be freely mutated and cause *Undefined Behavior*.
+		pub const unsafe fn as_mut_inner(&mut self) -> &mut $target {
+			&mut self.0
+		}
+	};
+}
+
+#[macro_export]
 macro_rules! transparent_wrapper {
 	{
 		$(#[$attr:meta])*
@@ -9,59 +68,7 @@ macro_rules! transparent_wrapper {
 		$vis struct $name($target);
 
 		impl $name {
-			/// Returns a mutable reference to a value of type
-			#[doc = concat!("[`", stringify!($name), "`]")]
-			/// given a raw pointer.
-			/// 
-			/// # Safety
-			/// `ptr` must point to a valid, mutable
-			#[doc = concat!("[`", $target_name, "`](", stringify!($target), ").")]
-			pub const unsafe fn from_mut_ptr<'a>(ptr: *mut $target) -> &'a mut Self {
-				unsafe { &mut *(ptr as *mut Self) }
-			}
-
-			/// Returns an immutable reference to a value of type
-			#[doc = concat!("[`", stringify!($name), "`]")]
-			/// given a raw pointer.
-			/// 
-			/// # Safety
-			/// `ptr` must point to a valid, immutable
-			#[doc = concat!("[`", $target_name, "`](", stringify!($target), ").")]
-			pub const unsafe fn from_ptr<'a>(ptr: *const $target) -> &'a Self {
-				unsafe { &*(ptr as *const Self) }
-			}
-
-			/// Returns the raw mutable pointer given a mutable reference to a value of type
-			#[doc = concat!("[`", stringify!($name), "`].")]
-			pub const fn as_mut_ptr(&mut self) -> *mut $target {
-				self as *mut Self as *mut $target
-			}
-
-			/// Returns the raw immutable pointer given an immutable reference to a value of type
-			#[doc = concat!("[`", stringify!($name), "`].")]
-			pub const fn as_ptr(&self) -> *const $target {
-				self as *const Self as *const $target
-			}
-
-			/// Returns an immutable reference to the inner C++ structure
-			/// given an immutable reference to a value of type
-			#[doc = concat!("[`", stringify!($name), "`].")]
-			pub const fn as_inner(&self) -> &$target {
-				&self.0
-			}
-
-			/// Returns a mutable reference to the inner C++ structure
-			/// given a mutable reference to a value of type
-			#[doc = concat!("[`", stringify!($name), "`].")]
-			///
-			/// See also [`as_inner`](Self::as_inner) for a safe version that doesn't permit mutation.
-			/// 
-			/// # Safety
-			/// The structure contains public fields for highly-specific applications,
-			/// which can be freely mutated and cause *Undefined Behavior*.
-			pub const unsafe fn as_mut_inner(&mut self) -> &mut $target {
-				&mut self.0
-			}
+			$crate::transparent_wrapper_impls!($name for $target as $target_name);
 		}
 	};
 }
