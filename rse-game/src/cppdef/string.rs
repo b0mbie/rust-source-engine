94bb5c5d81a9f4653eab6f749dfcc940c31208b4
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-09-26 02:51:13 +0300

    `rse-game`: Implement traits for `string_t` properly

--- rse-game/src/cppdef/string.rs
+++ rse-game/src/cppdef/string.rs
@@ -1,8 +1,14 @@
-use ::core::ffi::{
-	CStr, c_char,
+use ::core::{
+	cmp::Ordering,
+	ffi::{
+		CStr, c_char,
+	},
+	fmt,
+	hash::Hash,
+	ptr::null,
 };
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Default)]
 #[repr(C)]
 #[allow(non_camel_case_types)]
 pub struct string_t {
@@ -10,6 +16,10 @@ pub struct string_t {
 }
 
 impl string_t {
+	pub const NULL: Self = Self {
+		value: null(),
+	};
+
 	pub const fn new(s: &'static CStr) -> Self {
 		Self {
 			value: s.as_ptr(),
@@ -34,5 +44,34 @@ impl string_t {
 		} else {
 			unsafe { CStr::from_ptr(self.value) }
 		}
+	}
+}
+
+impl fmt::Debug for string_t {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		self.to_c_str().fmt(f)
+	}
+}
+
+impl Ord for string_t {
+	fn cmp(&self, other: &Self) -> Ordering {
+		self.to_c_str().cmp(other.to_c_str())
+	}
+}
+impl PartialOrd for string_t {
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		Some(self.cmp(other))
+	}
+}
+impl Eq for string_t {}
+impl PartialEq for string_t {
+	fn eq(&self, other: &Self) -> bool {
+		self.to_c_str().eq(other.to_c_str())
+	}
+}
+
+impl Hash for string_t {
+	fn hash<H: ::core::hash::Hasher>(&self, state: &mut H) {
+		self.to_c_str().hash(state)
 	}
 }
