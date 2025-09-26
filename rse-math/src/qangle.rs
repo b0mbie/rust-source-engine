ec25f00d34c13f640ed3f76d24c2d24e261d8a3e
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-09-26 05:02:44 +0300

    `rse-math`: Derive more traits

--- rse-math/src/qangle.rs
+++ rse-math/src/qangle.rs
@@ -2,7 +2,7 @@ use super::vec_t;
 
 /// Vector that represents three-dimensional extrinsic Tait-Bryan rotations following the right-hand rule,
 /// offset from the cardinal Z axis.
-#[derive(Default, Debug, Clone, Copy)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, PartialOrd)]
 #[repr(C)]
 pub struct QAngle {
 	pub x: vec_t,
