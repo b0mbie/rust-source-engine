4b754cc398f07c7539322c2fe06258a0a6ed7f8e
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-09-26 08:11:01 +0300

    `rse-convar`: Add argument iterator; add more docs

--- rse-convar/src/invocation.rs
+++ rse-convar/src/invocation.rs
@@ -24,22 +24,34 @@ use crate::cppdef::{
 impl Invocation {
 	pub const MAX_COMMAND_LENGTH: usize = COMMAND_MAX_LENGTH - 1;
 	
+	/// Returns the number of arguments.
 	pub const fn n_args(&self) -> usize {
 		self.0.argc as _
 	}
 
+	/// Returns an iterator over [`CStr`]s of arguments.
+	/// 
+	/// This is a convenience function for iterating over [`args`](Self::args)
+	/// while mapping them to C strings.
+	pub fn iter(&self) -> ArgIter<'_> {
+		Arg::iter(self.args())
+	}
+
+	/// Returns an immutable slice of all the [`Arg`]s.
 	pub const fn args(&self) -> &[Arg] {
 		unsafe {
 			from_raw_parts(self.0.argv.as_ptr() as *const Arg, self.n_args())
 		}
 	}
 
+	/// Returns a mutable slice of all the [`Arg`]s.
 	pub const fn args_mut(&mut self) -> &mut [Arg] {
 		unsafe {
 			from_raw_parts_mut(self.0.argv.as_mut_ptr() as *mut Arg, self.n_args())
 		}
 	}
 
+	/// Returns the whole argument string, if available.
 	pub fn arg_string(&self) -> Option<&CStr> {
 		if self.0.argc != 0 {
 			let c_str_bytes = unsafe {
@@ -57,20 +69,42 @@ impl Invocation {
 	}
 }
 
+/// Transparent wrapper around an [`Invocation`] argument C string.
 #[repr(transparent)]
 pub struct Arg {
 	ptr: *const c_char,
 }
 
 impl Arg {
+	/// Returns the [`CStr`] that is stored in this argument.
 	pub const fn as_c_str(&self) -> &CStr {
 		// SAFETY: The inner pointer is always a valid C string.
 		unsafe { CStr::from_ptr(self.ptr) }
+	}
+
+	/// Returns an iterator over [`Arg`] that maps each one to a [`CStr`].
+	pub fn iter(slice: &[Arg]) -> ArgIter<'_> {
+		ArgIter {
+			inner: slice.iter(),
+		}
 	}
 }
 
 impl fmt::Debug for Arg {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		self.as_c_str().fmt(f)
+	}
+}
+
+/// Iterator over [`Arg`]s that maps them to [`CStr`]s.
+#[repr(transparent)]
+pub struct ArgIter<'a> {
+	inner: ::core::slice::Iter<'a, Arg>,
+}
+impl<'a> Iterator for ArgIter<'a> {
+	type Item = &'a CStr;
+	fn next(&mut self) -> Option<Self::Item> {
+		let arg = self.inner.next()?;
+		Some(arg.as_c_str())
 	}
 }
