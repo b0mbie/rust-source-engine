9095c4502281b98f87ce2a011578486dc543ed89
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-10-01 02:07:10 +0300

    `rse-game-interfaces`: Fix C++ definitions for `cvar`

--- rse-game-interfaces/src/cppdef/cvar.rs
+++ rse-game-interfaces/src/cppdef/cvar.rs
@@ -2,18 +2,15 @@ use ::core::ffi::{
 	CStr, c_char, c_float, c_int,
 };
 use ::rse_convar::cppdef::{
-	ConVarVt, ConCommandBaseVt, ConCommandVt,
+	ConVar, ConCommandBaseVt, ConCommandVt,
 	CvarDllIdentifier,
+	FnChangeCallback,
 };
 use ::rse_cpp::{
 	vtable, RefConst, VtObjectMut, VtObjectRef, VtObjectPtr,
 };
 use ::rse_interface::cppdef::app_system::AppSystemVt;
 use ::rse_math::Color;
-
-pub type FnChangeCallback = unsafe extern "C" fn(
-	var: VtObjectMut<ConVarVt>, old_string: *const c_char, old_value: c_float,
-);
 
 pub const CVAR_INTERFACE_VERSION: &CStr = c"VEngineCvar004";
 vtable! {
@@ -25,15 +22,15 @@ vtable! {
 		pub fn get_command_line_value(name: *const c_char) -> *const c_char;
 		pub fn find_command_base(name: *const c_char) -> Option<VtObjectMut<ConCommandBaseVt>>;
 		pub fn find_command_base_const(name: *const c_char) -> Option<VtObjectRef<ConCommandBaseVt>>;
-		pub fn find_var(name: *const c_char) -> Option<VtObjectMut<ConVarVt>>;
-		pub fn find_var_const(name: *const c_char) -> Option<VtObjectRef<ConVarVt>>;
+		pub fn find_var(name: *const c_char) -> Option<*mut ConVar>;
+		pub fn find_var_const(name: *const c_char) -> Option<*const ConVar>;
 		pub fn find_command(name: *const c_char) -> Option<VtObjectMut<ConCommandVt>>;
 		pub fn find_command_const(name: *const c_char) -> Option<VtObjectRef<ConCommandVt>>;
 		pub fn get_commands() -> VtObjectMut<ConCommandBaseVt>;
 		pub fn get_commands_const() -> VtObjectRef<ConCommandBaseVt>;
 		pub fn install_global_change_callback(callback: FnChangeCallback);
 		pub fn remove_global_change_callback(callback: FnChangeCallback);
-		pub fn call_global_change_callbacks(var: VtObjectMut<ConVarVt>, old_string: *const c_char, old_value: c_float);
+		pub fn call_global_change_callbacks(var: *mut ConVar, old_string: *const c_char, old_value: c_float);
 		pub fn install_console_display_func(display_func: VtObjectMut<ConsoleDisplayFuncVt>);
 		pub fn remove_console_display_func(display_func: VtObjectMut<ConsoleDisplayFuncVt>);
 		pub fn console_color_printf(color: RefConst<Color>, format: *const c_char, ...);
@@ -42,9 +39,9 @@ vtable! {
 		pub fn revert_flagged_convars(flag: c_int);
 		pub fn install_cvar_query(query: VtObjectMut<CvarQueryVt>);
 		pub fn is_material_thread_set_allowed() -> bool;
-		pub fn queue_material_thread_set_value_string(convar: VtObjectMut<ConVarVt>, value: *const c_char);
-		pub fn queue_material_thread_set_value_int(convar: VtObjectMut<ConVarVt>, value: c_int);
-		pub fn queue_material_thread_set_value_float(convar: VtObjectMut<ConVarVt>, value: c_float);
+		pub fn queue_material_thread_set_value_string(convar: *mut ConVar, value: *const c_char);
+		pub fn queue_material_thread_set_value_int(convar: *mut ConVar, value: c_int);
+		pub fn queue_material_thread_set_value_float(convar: *mut ConVar, value: c_float);
 		pub fn has_queued_material_thread_convar_sets() -> bool;
 		pub fn process_queued_material_thread_convar_sets() -> c_int;
 	}
@@ -67,7 +64,7 @@ vtable! {
 pub const CVAR_QUERY_INTERFACE_VERSION: &CStr = c"VCvarQuery001";
 vtable! {
 	pub CvarQueryVtBase for VtObjectPtr<CvarQueryVt> {
-		pub fn are_convars_linkable(child: VtObjectRef<ConVarVt>, parent: VtObjectRef<ConVarVt>) -> bool;
+		pub fn are_convars_linkable(child: *const ConVar, parent: *const ConVar) -> bool;
 	}
 }
 
