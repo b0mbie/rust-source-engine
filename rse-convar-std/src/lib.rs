d272bf8487b96b78c46f743eef7ce6d5942ddd69
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-10-01 06:37:25 +0300

    `rse-convar-std`: Add `static_con_var`

--- rse-convar-std/src/lib.rs
+++ rse-convar-std/src/lib.rs
@@ -1,11 +1,23 @@
 #![no_std]
 
+use ::rse_convar::variable::{
+	low::StaticConVarObject,
+	Variable, params_for,
+};
+
 pub use ::rse_convar as convar;
 
 mod c_buffer;
 use c_buffer::*;
 mod variable;
 pub use variable::*;
+
+pub const fn static_con_var<T>(variable: T) -> StaticConVarObject<StdVariable<T>>
+where
+	T: Variable,
+{
+	StaticConVarObject::new(StdVariable::new(variable), params_for::<T>())
+}
 
 pub mod prelude {
 	pub use ::rse_convar::prelude::*;
