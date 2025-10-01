2bd23b6e5bd2836c70ba307154e52f61e304f520
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-10-01 06:01:17 +0300

    `rse-convar`: Add auto-complete support to `Command`

--- rse-convar/src/command/mod.rs
+++ rse-convar/src/command/mod.rs
@@ -9,6 +9,8 @@ use crate::{
 
 pub mod low;
 
+pub use low::Suggestions;
+
 #[cfg(feature = "macros")]
 mod macros;
 
@@ -28,30 +30,35 @@ pub unsafe trait Command {
 	const HELP: Option<&CStr>;
 	fn dispatch(&mut self, invocation: &Invocation);
 
+	fn can_auto_complete(&mut self) -> bool {
+		false
+	}
+	fn auto_complete(&mut self, partial: &CStr, suggestions: &mut Suggestions) {
+		let _ = partial;
+		let _ = suggestions;
+	}
+
 	fn dll_identifier(&mut self) -> CvarDllIdentifier;
 }
 
 unsafe impl<'a, T: Command> low::RawCommand<'a> for T {
 	fn name(object: &mut low::ConCommandObject<'a, Self>) {
-		unsafe { object.as_mut_base().as_mut_inner().name = T::NAME.as_ptr() }
+		let _ = object;
+		// unsafe { object.as_mut_base().as_mut_inner().name = T::NAME.as_ptr() }
 	}
 	fn dispatch(object: &mut low::ConCommandObject<'a, Self>, invocation: &Invocation) {
 		object.inner.dispatch(invocation)
 	}
 	fn can_auto_complete(object: &mut low::ConCommandObject<'a, Self>) -> bool {
-		// TODO: Auto-complete.
-		let _ = object;
-		true
+		object.inner.can_auto_complete()
 	}
 	fn auto_complete_suggest(
 		object: &mut low::ConCommandObject<'a, Self>,
 		partial: &CStr,
-		mut suggestions: low::Suggestions<'_>,
+		suggestions: &mut Suggestions,
 	) -> low::SuggestionCount {
-		// TODO: Auto-complete.
-		let _ = object;
-		suggestions.try_push(low::CString::from(partial));
-		low::SuggestionCount::ONE
+		object.inner.auto_complete(partial, suggestions);
+		suggestions.count()
 	}
 }
 unsafe impl<'a, T: Command> crate::console_base::RawConsoleBase<low::ConCommandObject<'a, T>> for T {
@@ -59,7 +66,8 @@ unsafe impl<'a, T: Command> crate::console_base::RawConsoleBase<low::ConCommandO
 		let _ = object;
 	}
 	fn help(object: &mut low::ConCommandObject<'a, T>) {
-		unsafe { object.as_mut_base().as_mut_inner().help_string = crate::util::c_str_ptr(T::HELP) }
+		let _ = object;
+		// unsafe { object.as_mut_base().as_mut_inner().help_string = crate::util::c_str_ptr(T::HELP) }
 	}
 	fn dll_identifier(object: &mut low::ConCommandObject<'a, T>) -> CvarDllIdentifier {
 		object.inner.dll_identifier()
