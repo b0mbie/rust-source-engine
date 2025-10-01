993113ae2cafee164ca132ca88d1183da6430097
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-10-01 03:54:17 +0300

    `rse-convar`: Fix `RawVariable` function signatures

--- rse-convar/src/variable/low/mod.rs
+++ rse-convar/src/variable/low/mod.rs
@@ -16,13 +16,13 @@ where
 	Self: Sized,
 	Self: RawConsoleBase<ConVarObject<'a, Self>>,
 {
-	fn set_c_str(object: &mut ConVarObject<Self>, value: Option<&CStr>);
-	fn set_float_forced(object: &mut ConVarObject<Self>, value: c_float);
-	fn set_int(object: &mut ConVarObject<Self>, value: c_int);
+	fn set_c_str(object: &mut ConVarObject<'a, Self>, value: Option<&CStr>);
+	fn set_float_forced(object: &mut ConVarObject<'a, Self>, value: c_float);
+	fn set_int(object: &mut ConVarObject<'a, Self>, value: c_int);
 
-	fn set_float(object: &mut ConVarObject<Self>, value: c_float) {
+	fn set_float(object: &mut ConVarObject<'a, Self>, value: c_float) {
 		Self::set_float_forced(object, value)
 	}
 
-	fn change_string_value(object: &mut ConVarObject<Self>, new_value: Option<&CStr>, old_value: c_float);
+	fn change_string_value(object: &mut ConVarObject<'a, Self>, new_value: Option<&CStr>, old_value: c_float);
 }
