741869309dbe56cf63b472ab09c51e1a7dc679e2
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-10-02 03:16:23 +0300

    `rse-game-interfaces`: Add `CvarImpl::register`, `unregister(_all)`

--- rse-game-interfaces/src/cvar.rs
+++ rse-game-interfaces/src/cvar.rs
@@ -1,6 +1,11 @@
 use ::core::ffi::CStr;
-use ::rse_convar::cppdef::CvarDllIdentifier;
-use ::rse_cpp::AsObject;
+use ::rse_convar::console_base::{
+	CvarDllIdentifier, AsConCommandBase,
+};
+use ::rse_cpp::{
+	AsObject,
+	virtual_call,
+};
 
 use crate::cppdef::{
 	CVAR_INTERFACE_VERSION, CvarVt,
@@ -10,19 +15,38 @@ use super::{
 	InterfaceOfFactory, AppSystemFactory,
 };
 
-macro_rules! cvar_call {
-	($object:expr => $field:ident($($arg:tt)*)) => {{
-		let object = $object;
-		(object.vtable().cvar.$field)(object.as_ptr().cast(), $($arg)*)
-	}};
-}
-
 pub trait CvarImpl: AsObject<CvarVt> {
 	fn allocate_dll_identifier(&mut self) -> CvarDllIdentifier {
-		unsafe { cvar_call!(self.as_object() => allocate_dll_identifier()) }
+		unsafe { virtual_call!(self.as_object() => cvar.allocate_dll_identifier()) }
 	}
 	fn is_material_thread_set_allowed(&self) -> bool {
-		unsafe { cvar_call!(self.as_object() => is_material_thread_set_allowed()) }
+		unsafe { virtual_call!(self.as_object() => cvar.is_material_thread_set_allowed()) }
+	}
+
+	/// # Safety
+	/// Registered console commands and variables *must* eventually be unregistered with
+	/// [`unregister`](CvarImpl::unregister) or [`unregister_all`](CvarImpl::unregister_all).
+	unsafe fn register<C>(&mut self, command: &mut C)
+	where
+		C: AsConCommandBase,
+	{
+		unsafe { virtual_call!(self.as_object() => cvar.register_con_command(command.as_con_command_base())) }
+	}
+
+	/// # Safety
+	/// `command` must have been registered with this interface.
+	unsafe fn unregister<C>(&mut self, command: &mut C)
+	where
+		C: AsConCommandBase,
+	{
+		unsafe { virtual_call!(self.as_object() => cvar.unregister_con_command(command.as_con_command_base())) }
+	}
+
+	/// # Safety
+	/// `dll_identifier` must be a [`CvarDllIdentifier`] that was returned by a call to
+	/// [`allocate_dll_identifier`](CvarImpl::allocate_dll_identifier).
+	unsafe fn unregister_all<C>(&mut self, dll_identifier: CvarDllIdentifier) {
+		unsafe { virtual_call!(self.as_object() => cvar.unregister_con_commands(dll_identifier)) }
 	}
 }
 impl<T: AsObject<CvarVt>> CvarImpl for T {}
