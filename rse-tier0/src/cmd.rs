3826f0ff5d97c23b3f7d0a2016c8669cc50a4614
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-11-03 02:50:50 +0200

    `rse-tier0`: Add blanket `Tier0CommandLine` impl for `&T`

--- rse-tier0/src/cmd.rs
+++ rse-tier0/src/cmd.rs
@@ -24,3 +24,33 @@ pub trait Tier0CommandLine {
 	fn get_parm(&self, index: c_int) -> &CStr;
 	fn has_parm(&self, s: &CStr) -> bool;
 }
+
+impl<T: ?Sized + Tier0CommandLine> Tier0CommandLine for &T {
+	fn check_parm<'a>(&'a self, key: &CStr) -> Option<&'a CStr> {
+		T::check_parm(self, key)
+	}
+	fn parm_str<'a>(&'a self, key: &CStr) -> Option<&'a CStr> {
+		T::parm_str(self, key)
+	}
+	fn parm_str_or<'a, 'def: 'a>(&'a self, key: &CStr, default: &'def CStr) -> &'def CStr {
+		T::parm_str_or(self, key, default)
+	}
+	fn parm_int_or(&self, key: &CStr, default: c_int) -> c_int {
+		T::parm_int_or(self, key, default)
+	}
+	fn parm_float_or(&self, key: &CStr, default: c_float) -> c_float {
+		T::parm_float_or(self, key, default)
+	}
+	fn parm_count(&self) -> c_int {
+		T::parm_count(self)
+	}
+	fn find_parm(&self, s: &CStr) -> Option<NonZero<c_int>> {
+		T::find_parm(self, s)
+	}
+	fn get_parm(&self, index: c_int) -> &CStr {
+		T::get_parm(self, index)
+	}
+	fn has_parm(&self, s: &CStr) -> bool {
+		T::has_parm(self, s)
+	}
+}
