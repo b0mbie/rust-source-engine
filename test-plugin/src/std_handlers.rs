78afdd9b344eb7e9daf0e774514c0bff3c5d19cc
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-11-08 03:35:41 +0200

    Remove all of the test plugins for now
    There should be separate examples that show off specific functionality, instead.

--- test-plugin/src/std_handlers.rs
+++
@@ -1,7 +0,0 @@
-use ::rse_tier0::{
-	linked::mem::LinkedTier0Allocator,
-	mem_alloc::Tier0GlobalAlloc,
-};
-
-#[global_allocator]
-static ALLOCATOR: Tier0GlobalAlloc<LinkedTier0Allocator> = Tier0GlobalAlloc(LinkedTier0Allocator);
