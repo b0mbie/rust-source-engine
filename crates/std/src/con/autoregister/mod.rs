985774bc85a1da38ec7a125750838d8817a55733
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-12-08 06:51:38 +0200

    `rse-std`: Don't use `dyn AsStaticRegistrable` for `add_to_register`

--- crates/std/src/con/autoregister/mod.rs
+++ crates/std/src/con/autoregister/mod.rs
@@ -14,11 +14,14 @@ use super::cvar::register_raw;
 mod macros;
 pub use macros::*;
 
-pub fn add_to_register(registrable: &'static dyn AsStaticRegistrable) {
-	let mut new_first = registrable.as_static_registrable();
-	let old_first = FIRST_REGISTRABLE.swap(null_mut(), Ordering::Acquire);
-	unsafe { new_first.as_mut_base().data.next = old_first }
-	FIRST_REGISTRABLE.store(new_first.as_ptr().as_ptr(), Ordering::Release);
+pub fn add_to_register<T: ?Sized + AsStaticRegistrable>(registrable: &'static T) {
+	fn add_to_register_impl(registrable: StaticRegistrable) {
+		let mut new_first = registrable;
+		let old_first = FIRST_REGISTRABLE.swap(null_mut(), Ordering::Acquire);
+		unsafe { new_first.as_mut_base().data.next = old_first }
+		FIRST_REGISTRABLE.store(new_first.as_ptr().as_ptr(), Ordering::Release);
+	}
+	add_to_register_impl(registrable.as_static_registrable())
 }
 
 pub fn register_all() {
@@ -43,7 +46,10 @@ impl AsStaticRegistrable for crate::con::cmd::ConCommand {
 		unsafe { StaticRegistrable::new(self.as_registrable()) }
 	}
 }
-impl<T> AsStaticRegistrable for crate::con::cmd::GenericConCommand<'_, T> {
+impl<'str, T> AsStaticRegistrable for crate::con::cmd::GenericConCommand<'str, T>
+where
+	T: crate::con::cmd::GenericCommand<'str>,
+{
 	fn as_static_registrable(&'static self) -> StaticRegistrable {
 		unsafe { StaticRegistrable::new(self.as_registrable()) }
 	}
